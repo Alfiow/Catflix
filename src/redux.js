8d7a55b7f0d28585808a17a355bb5937c41f824e
Alfiow <alfianan0412@bsi.ac.id>
2017-10-14 18:08:42 +0700

    configure the reducer

--- src/redux.js
+++ src/redux.js
@@ -16,3 +16,38 @@ export const initialState = {
   next: [],
 };
 
+// Reducer
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    // Handle API response with data object that looks like { gif: 'data:image/gif;base64,...' }
+    case 'IMAGE_LOADED':
+      return {
+        ...state,
+        // update current image if it's empty
+        image: state.image || action.data,
+        next: [
+          action.data,              // put image an the beginning of next array
+          ...state.next,            // put the existing images after
+        ]
+      };
+    // Change current image with the next one
+    case 'CHANGE_IMAGE':
+      // Do nothing if next array is empty.
+      // Just keep the current image till the new one is fetched.
+      if (!state.next.length) {
+        return state;
+      }
+      return {
+        ...state,                   // keep the existing state,
+        image: head(state.next),    // get fist item from next array
+        next: [
+          ...slice(state.next, 1),  // drop first item from next array
+        ],
+      };
+    // Handle API request errors
+    case 'ERROR':
+      return state;                 // do nothing
+    default:
+      return state;
+  }
+};
